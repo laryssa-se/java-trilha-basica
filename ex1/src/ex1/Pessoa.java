5daae80fec24d652295d01805627ebfe3751bfb6
laryssa-serra <se.laryssa@gmail.com>
2021-08-04 18:05:18 -0300

    adicionado formatador automatico

--- ex1/src/ex1/Pessoa.java
+++ ex1/src/ex1/Pessoa.java
@@ -7,34 +7,40 @@ public class Pessoa {
 	private String name;
 	private LocalDate birthDate;
 	private double height;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
+
 	public double getHeight() {
 		return height;
 	}
+
 	public void setHeight(double height) {
 		this.height = height;
 	}
-	
-	public void info () {
-		System.out.println("Name: " + getName() + "\n" + "Birthday: " + getBirthDate() + "\n" + "Height: " + getHeight());
+
+	public void info() {
+		System.out
+				.println("Name: " + getName() + "\n" + "Birthday: " + getBirthDate() + "\n" + "Height: " + getHeight());
 	}
-	
+
 	public int calculateAge() {
 		LocalDate now = LocalDate.now();
 		Period diff = Period.between(getBirthDate(), now);
-	
-		return diff.getYears();		
-	}	
+
+		return diff.getYears();
+	}
 }
